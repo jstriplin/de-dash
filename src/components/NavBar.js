d430ede196c7a745f841209e40ca854939af0619
jared striplin <jstriplin@fubo.tv>
2021-02-01 12:00:26 -0500

    build NavBar component

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/NavBar.css";
+
+function NavBar() {
+  return (
+    <div className="navbar-container">
+      <h1 className="navbar-title"> De Dash </h1>
+    </div>
+  );
+}
+
+export default NavBar;
